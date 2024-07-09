4db42e8ad8f798f87822c31701c4bd7dc1ee1a62
Shushank Gautam <shushank.g@gsl.in>
2024-07-09 21:12:16 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>DC Heroes</h1>
+    <ul>
+      <li v-for="hero in name" :key="hero">
+        {{ hero }}
+      </li>
+    </ul>
+    <button :disabled="isActive">Add Hero</button>
+    <input type="text" :placeholder="placeholderValue">
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: ["superman", "batman", "flash"],
+      isActive:false,
+      placeholderValue:"Hero-Name"
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add your styles here */
 </style>
